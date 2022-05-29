8a2661c90dee8be848821faa6199864e2eb93754
LordJonny1000 <101137406+LordJonny1000@users.noreply.github.com>
2022-05-29 22:41:15 +0200

    Delete locations.py

--- vocabulary/locations.py
+++
@@ -1,9 +0,0 @@
-list_of_locations = [
-["Keller", "weak", "masculine", False],
-["Flughafen", "strong", "masculine", False],
-["Disco", "weak", "feminine", False],
-["Boxring", "weak", "masculine", False],
-["Eckkneipe", "weak", "feminine", False],
-["Hölle", "weak", "feminine", False],
-["Nirvana", "weak", "neutral", False],
-]
